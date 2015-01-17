64f95530de942953fe8386d57ec4579c389b94fd
Karen Javadyan <kajarenc@yandex.ru>
2015-01-17 14:06:11 -0800

    added test poll app from django tutorials

--- DjangoUnchained/settings.py
+++ DjangoUnchained/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tarantinoApp',
+    'polls',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ polls/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from polls.models import Question, Choice
+
+# Register your models here.
+
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
